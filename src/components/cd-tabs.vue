cdb321db124fdaeb545f969b1d5675e0ac6406a2
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-17 18:11:14 +0300

    cd-tabs

--- src/components/cd-tabs.vue
+++ src/components/cd-tabs.vue
@@ -1,5 +1,14 @@
 <template>
-  <cd-list class="cd-tabs"></cd-list>
+  <cd-list class="cd-tabs" :collection="tabs" :key-field="tabKey" list-class="cd-tabs--wrap" row-class="cd-tab--wrap">
+    <div class="cd-tab" slot-scope="{ row, index }" :class="[row.class, { 'is-disabled pe-none': resolveTabDisabled(row, index) }]">
+      <slot :tab="row" :index="index">
+
+      </slot>
+    </div>
+    <div class="cd-tabs--content" slot="footer">
+      <slot name="content"></slot>
+    </div>
+  </cd-list>
 </template>
 
 <script>
@@ -8,6 +17,23 @@ export default {
   name: 'cd-tabs',
   components: {
     'cd-list': CDList
+  },
+  props: {
+    tabs: { type: Array, required: true, description: 'Вкладки' },
+    tabCaption: { type: String, description: 'Свойство объекта из массива [tabs], в котором находится текст вкладки' },
+    tabKey: { type: String, required: true, default: 'key', description: 'Свойство объекта из массива [tabs], в котором находится идентификатор вкладки' },
+    isTabDisabled: { type: Function, description: 'Вычисляем для объекта вкладки и её индекса в массиве [tabs], будет ли вкладка задисаблена' }
+  },
+  computed: {
+    resolveTabDisabled () {
+      const isTabDisabled = this.isTabDisabled
+      const isFunction = typeof isTabDisabled === 'function'
+      return (row, index) => isTabDisabled === undefined 
+      ? false 
+      : isFunction 
+        ? isTabDisabled(row, index) 
+        : isTabDisabled
+    }
   }
 }
 </script>
--- tests/unit/cd-tabs.spec.js
+++ tests/unit/cd-tabs.spec.js
@@ -1,9 +1,50 @@
 import CDTabs from '@/components/cd-tabs'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+const tabs = [{ datafield: 'xyz', class: 'xyz-class' }, { datafield: 'yzx' }, { datafield: 'zxy' }]
+const basicsMount = {
+  tabs,
+  tabKey: 'datafield',
+  tabCaption: 'datafield',
+  isTabDisabled: (tab, index) => {
+    return tab.datafield === 'xyz'
+  }
+}
 
-describe('[cd-tabs]', () => {
+
+describe('[cd-tabs] basics', () => {
   it('is mounted', () => {
-    const wrapper = shallowMount(CDTabs)
+    const wrapper = mount(CDTabs, { propsData: basicsMount })
     expect(wrapper.findAll('.cd-tabs').length).toBe(1)
   })
+  it ('has content-slot', () => {
+    const wrapper = mount(CDTabs, { propsData: basicsMount })
+    expect(wrapper.findAll('.cd-tabs--content').length).toBe(1)
+  })
+  it ('has one [.cd-tabs--wrapper]', done => {
+    const wrapper = mount(CDTabs, { propsData: basicsMount })
+    Vue.nextTick().then(() => {
+      expect(wrapper.findAll('.cd-tabs--wrap').length).toBe(1)
+      done()
+    })
+  })
+  it (`has ${tabs.length} tabs`, done => {
+    const wrapper = mount(CDTabs, { 
+      propsData: basicsMount, 
+      scopedSlots: { 
+        default: '<p class="cd-tab--test" slot-scope="row">{{ row }}</p>',
+        content: '<p class="cd-tab--content">A</p>'
+      } 
+    })
+    Vue.nextTick().then(() => {
+      const tabwraps = 
+      expect(wrapper.findAll('li').length).toBe(tabs.length)
+      expect(wrapper.findAll('.cd-tab--wrap').length).toBe(tabs.length)
+      expect(wrapper.findAll('.cd-tab').length).toBe(tabs.length)
+      expect(wrapper.findAll('.cd-tab--test').length).toBe(tabs.length)
+      expect(wrapper.find('.cd-tab--content').text()).toBe('A')
+      expect(wrapper.findAll('.is-disabled').length).toBe(tabs.filter((tab, index) => basicsMount.isTabDisabled(tab, index)).length)
+      done()
+    })
+  }, 10000)
 })
