273c31d7d6a3635fd95d3a2e06c3841a3785dea2
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-19 10:45:12 +0300

    flatterer

---
+++ src/common/property-flatter.js
@@ -0,0 +1,18 @@
+const hasArray = (payload, property) => Object.prototype.hasOwnProperty.call(payload, property) && Array.isArray(payload[property])
+import propertyDecorator from './property-decorator'
+export default function flatterer (arr, accum) {
+    return (arr || []).reduce((acc, u) => { // выбираем между arr и пустым
+      // массивом и выполняем над ним reduce
+      // функцией (acc, u) => {},
+      // где acc -- массив-аккумулятор, а u -- текущее свойств
+      let res = []
+      if (hasArray(u, 'descriptor')) {
+        // соединим массив acc с результатом выполнения функции reducer над u.descriptor
+        res = [u].concat(acc.concat(flatterer(u.descriptor, [])))
+      } else {
+        acc.push(u)
+        res = acc
+      }
+      return res // вернём что получилось
+    }, accum)
+  }
