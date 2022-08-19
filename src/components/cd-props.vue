e98aa0b3e035e680cf826d68cefcf7bcd7150131
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-19 11:33:57 +0300

    cd-props

--- src/components/cd-props.vue
+++ src/components/cd-props.vue
@@ -1,7 +1,17 @@
 <template>
-  <cd-list class="cd-props" :collection="descriptor" key-field="datafield" >
+  <cd-list class="cd-props" :collection="descriptor" key-field="datafield" :is-row-visible="isRowVisible">
     <template v-if="$slots.header" slot="header">
       <slot name="header"></slot>
+    </template>
+    <template slot-scope="{ row, index }">
+      <slot :property="row" :index="index">
+        <template v-if="hasDescriptor(row)">
+          <cd-props :class="row.class" :descriptor="row.descriptor" :payload="payload"></cd-props>
+        </template>
+        <template v-else>
+          {{ row }}
+        </template>
+      </slot>
     </template>
     <template v-if="$slots.footer" slot="footer">
       <slot name="footer"></slot>
@@ -10,11 +20,31 @@
 </template>
 
 <script>
+import CDList from './cd-list.vue'
+import decorator from '@/common/property-decorator'
 export default {
   name: 'cd-props',
+  components: {
+    'cd-list': CDList
+  },
   props: {
     descriptor: { type: Array, required: true, description: 'Массив свойств' },
-    payload: { type: Object, required: true, description: 'Объект, свойства которого мы рассматриваем' }
+    payload: { type: Object, required: true, description: 'Объект, свойства которого мы рассматриваем' },
+    isRowVisible: { 
+      type: Function, 
+      default: function (row, index) {
+        return decorator.isVisible(row, this.payload)
+      }
+    }
+  },
+  computed: {
+    hasDescriptor () {
+      return (property) => decorator.hasDescriptor(property)
+    },
+    isRowVisible () {
+      const props = this
+      return (property) => decorator.isVisible(property, props.payload)
+    }
   }
 }
 </script>
--- tests/unit/cd-props.spec.js
+++ tests/unit/cd-props.spec.js
@@ -1,12 +1,28 @@
 import CDProps from '@/components/cd-props'
 import { mount } from '@vue/test-utils'
 import Vue from 'vue'
-
+import descriptor from './descriptors'
+console.log(descriptor)
 describe('cd-props', () => {
+ 
+  const propsData = {
+    descriptor:  descriptor.objectDescriptor,
+    payload: {}
+  }
   it ('cd-props is mounted', (done) => {
-    const wrapper = mount(CDProps)  
+    const wrapper = mount(CDProps, { propsData })  
     Vue.nextTick().then(() => {
-      expect(wrapper.findAll('.cd-props').length).toBe(1)
+      expect(wrapper.findAll('.cd-props').length).toBe(26)
+      done()
+    })
+  })
+  it ('cd-props is mounted with properties', (done) => {
+    const wrapper = mount(CDProps, { propsData })
+    Vue.nextTick().then(() => {
+      const list_of_list = wrapper.findAll('.cd-list')
+      expect(list_of_list.length).toBe(26)
+      expect(wrapper.findAll('li').length).toBeGreaterThan(0)
+      expect(wrapper.findAllComponents(CDProps).length).toBeGreaterThan(12)
       done()
     })
   })
