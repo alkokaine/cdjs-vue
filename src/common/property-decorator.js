7cca857f017344b4b07ef05e58c598a4fe2bd88c
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-28 03:03:18 +0300

    isPropertyVisible

--- src/common/property-decorator.js
+++ src/common/property-decorator.js
@@ -17,6 +17,7 @@ const is = (propertyname, property, payload) => {
 const isHidden = (property, payload) => is('hidden', property, payload)
 const isVisible = (property, payload) => !isHidden(property, payload)
 const isEditable = (property, payload) => is('canedit', property, payload)
+const isPropertyVisible = (property, payload, index) => isVisible(property, payload, index) && !isHidden(property, payload, index)
 export default {
-    hasDescriptor, propertyKey, hasLegend, isHidden, isVisible, isEditable
+    hasDescriptor, propertyKey, hasLegend, isHidden, isVisible, isEditable, isPropertyVisible
 }
