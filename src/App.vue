75c588657dbf26ed02a3c4b9ed1d5f11be96b468
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-28 03:17:48 +0300

    visual testing

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app" class="main-app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <div v-if="payload">
+      <cd-props :descriptor="descriptor" :payload="payload"></cd-props>
+      <cd-form class="text-start w-auto" :descriptor="descriptor" :payload="payload"></cd-form>
+    </div>
   </div>
 </template>
 
 <script>
+import example from '@/assets/descriptors'
+import CdProps from './components/cd-props.vue'
+import CdForm from './components/cd-form.vue'
 export default {
   name: 'App',
+  components: {
+    'cd-props': CdProps,
+    'cd-form': CdForm
+  },
+  data () {
+    return {
+      descriptor: example.objectDescriptor,
+      payload: example.object
+    }
+  }
 }
 </script>
 
