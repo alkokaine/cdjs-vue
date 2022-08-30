72d6e5979dff5486bb879ae2eafae3ba35d37506
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-30 12:55:05 +0300

    grid view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app" class="main-app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <cd-grid :collection="collection" key-field="ObjectID" :descriptor="columns"/>
   </div>
 </template>
 
 <script>
+import descriptor from '@/assets/descriptors'
+import CDGrid from '@/components/cd-grid.vue'
 export default {
   name: 'App',
+  components: {
+    'cd-grid': CDGrid
+  },
+  data (app) {
+    return {
+      collection: [descriptor.object, descriptor.object, descriptor.object],
+      columns: descriptor.objectDescriptor
+    }
+  }
 }
 </script>
 
