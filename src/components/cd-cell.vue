4203c3e347a40ccd39047b5b391a7476d8e9c64d
sit19 <alkokaine@yandex.ru>
2022-08-30 06:53:33 +0300

    autocomplete: fetch

--- src/components/cd-cell.vue
+++ src/components/cd-cell.vue
@@ -1,9 +1,40 @@
 <template>
   <div class="cd-cell pt-2" :class="cellClassResolved">
-    <label class="cd-label pe-2 text-nowrap" slot="label">{{ property.text }}</label>
-    <div class="cd-field--input">
-      <el-input class="el-input w-auto" :value="value"/>
+    <template v-if="$slots.header">
+      <slot name="header"></slot>
+    </template>
+    <slot name="label">
+      <label class="cd-label pe-2 text-nowrap">{{ property.text }}</label>
+    </slot>
+    <div class="cd-field--input" :class="[{ 'w-100': !input.checkbox, 'cd-checkbox': input.checkbox }]">
+      <template v-if="input.number"></template>
+      <template v-else-if="input.select">
+        <el-select :value="value"></el-select>
+      </template>
+      <template v-else-if="input.autocomplete">
+        <el-autocomplete :value="value" :fetch-suggestions="fetch"></el-autocomplete>
+      </template>
+      <template v-else-if="input.textarea">
+        <el-input type="textarea" :value="value"></el-input>
+      </template>
+      <template v-else-if="input.checkbox">
+        <el-checkbox :value="value"></el-checkbox>
+      </template>
+      <template v-else-if="input.date"></template>
+      <template v-else-if="input.datetime"></template>
+      <template v-else-if="input.email"></template>
+      <template v-else-if="input.radio"></template>
+      <template v-else-if="input.range"></template>
+      <template v-else-if="input.tel"></template>
+      <template v-else-if="input.url"></template>
+      <template v-else-if="input.file"></template>
+      <template v-else>
+        <el-input class="el-input" :value="value"/>
+      </template>
     </div>
+    <template v-if="$slots.footer">
+      <slot name="footer"></slot>
+    </template>
   </div>
 </template>
 <script>
@@ -12,15 +43,43 @@
     props: {
       cellClass: { type: [Array, Object, String, Function] },
       property: { type: Object, default: Object },
-      value: { type: [Array, String, Number, Boolean]}
+      value: { type: [Array, String, Number, Boolean] }
     },
     data (cell) {
-      return {}
+      const input = cell.property.input
+      return {
+        input: {
+          select: input === 'select',
+          autocomplete: input === 'autocomplete',
+          number: input === 'number',
+          textarea: input === 'textarea',
+          checkbox: input === 'checkbox',
+          date: input === 'date',
+          datetime: input === 'datetime',
+          email: input === 'email',
+          radio: input === 'radio',
+          range: input === 'range',
+          tel: input === 'tel',
+          text: input === 'text',
+          url: input === 'url',
+          file: input === 'file'
+        }
+      }
     },
     computed: {
       cellClassResolved () {
         return 'cd-cell--resolved'
       }
+    },
+    methods: {
+      fetch (query, callback) {
+        callback([])
+      }
     }
   }
 </script>
+<style>
+  .cd-checkbox {
+    max-width: min-content;
+  }
+</style>
