353a47f096f9545b27c948be781525a69b63841b
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-31 19:19:09 +0300

    tests

--- tests/unit/cd-day.spec.js
+++ tests/unit/cd-day.spec.js
@@ -1,9 +1,9 @@
 import CDDay from '@/components/cd-day'
-import { shallowMount } from '@vue/test-utils'
-
+import { mount } from '@vue/test-utils'
+import { createDate } from '@/common/month-days'
 describe('[cd-day]', () => {
   it('is mounted', () => {
-    const wrapper = shallowMount(CDDay)
+    const wrapper = mount(CDDay, { propsData: { day: createDate(2022, 9, 1).toDate() } })
     expect(wrapper.findAll('.cd-day').length).toBe(1)
   })
 })
--- tests/unit/cd-month.spec.js
+++ tests/unit/cd-month.spec.js
@@ -1,9 +1,12 @@
 import CDDay from '@/components/cd-month'
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+import { createDate, prevMonthWeekLength } from '@/common/month-days'
 
 describe('[cd-mount]', () => {
   it('is mounted', () => {
-    const wrapper = shallowMount(CDDay)
+    const wrapper = mount(CDDay, { propsData: { 
+      date: createDate(2022, 9, 1).toDate()
+    }})
     expect(wrapper.findAll('.cd-month').length).toBe(1)
   })
 })
