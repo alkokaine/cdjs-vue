62b41b58fef64de745442b72573d6322fe43fb4d
Алексей Коковин <KokovinAA@cross-d.ru>
2022-08-31 20:40:22 +0300

    cd-day: begin

--- src/components/cd-day.vue
+++ src/components/cd-day.vue
@@ -1,13 +1,50 @@
 <template>
-  <div class="cd-day"></div>
+  <div class="cd-day px-3 my-1">
+    <div class="cd-day--header" :class="{ 'is-prev': day.isprev, 'is-eve': day.code === 2, 'holiday': day.code === 1 }">
+      <slot name="header">
+        <span class="cd-day--block">{{ number }}</span>
+      </slot>
+    </div>
+    <template v-if="$slots.default">
+      <slot></slot>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'cd-day'
+  name: 'cd-day',
+  props: {
+    day: { type: Object, required: true }
+  },
+  data (day) {
+    return {
+      number: day.day.date.date()
+    }
+  }
 }
 </script>
 
 <style>
-
+  .cd-day--header {
+    font-size: 0.8rem;
+  }
+  .is-prev {
+    opacity: 42%;
+    pointer-events: none;
+    cursor: default;
+    user-select: none;
+  }
+  .cd-days--container {
+    max-width: min-content;
+  }
+  .is-eve {
+    opacity: 70%;
+  }
+  .cd-day {
+    cursor: pointer;
+  }
+  .cd-day:hover {
+    background-color: #f1ebeb;
+  }
 </style>
