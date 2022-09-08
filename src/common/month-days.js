4f253498c1de4fda400d956227eea1aa3b1145e4
Алексей Коковин <KokovinAA@cross-d.ru>
2022-09-08 19:36:05 +0300

    month-days, some changes

--- src/common/month-days.js
+++ src/common/month-days.js
@@ -10,17 +10,36 @@ const prevMonthWeekLength = (date) => {
   return (lastMonthDay(prev.year(), prev.month() + 1)).day()
 }
 const prevMonthDays = (date) => {
-  const prevmonth = (moment(date)).subtract(1, 'month')
+  const prevmonth = date.subtract(1, 'month')
   const lastDay = lastMonthDay(prevmonth.year(), prevmonth.month() + 1)
   const days = []
   while (lastDay.day() > 0) {
-    days.unshift({ date: moment(lastDay.toDate()), isprev: true })
+    days.unshift({ day: lastDay.day(), number: lastDay.date(), date: lastDay.toISOString(true), isprev: true })
     lastDay.subtract(1, 'days')
   }
   return days
 }
+const weekdays = (
+  [1, 2, 3, 4, 5, 6, 7]
+    .map(d => ({
+      weekday: {
+        short: moment().isoWeekday(d).format('dd'),
+        long: moment().isoWeekday(d).format('dddd'),
+      },
+      order: d,
+      day: d % 7
+    })
+))
+
+function getMonthDays(moment) {
+  const days_in_month_keys = Array(moment.daysInMonth()).keys()
+  const array_from_keys = Array.from(days_in_month_keys)
+  const result = array_from_keys.map((m) => (createDate(moment.year(), moment.month() + 1, m + 1) )).map(date => ({ day: date.day(), month: date.month(), number: date.date(), date: date.toISOString(true) }))
+  return result
+}
+
 const getDays = (date, options = { pre: 1, covid: 1, sd: 0 }) => {
-  const days = []
+  // const days = []
   const params = {
     year: date.getFullYear(),
     month: date.getMonth() + 1,
@@ -28,47 +47,38 @@ const getDays = (date, options = { pre: 1, covid: 1, sd: 0 }) => {
     covid: options.covid,
     sd: options.sd
   }
-  const createDay = (index, code) => {
+  const createDay = (index) => {
     const date = createDate(params.year, params.month, index + 1)
-    if (isNaN(code)) {
-      if (options.sd) return  { date, code: date.day() === 0 }
-      else return { date, code: [0, 6].indexOf(date.day()) >= 0 }
-    }
-    return { date, code }
+    if (options.sd) return  { date, code: date.day() === 0 }
+    else return { date, code: [0, 6].indexOf(date.day()) >= 0 }
   }
-  return new Promise((resolve, reject) => {
-    axios({
-      url: 'https://isdayoff.ru/api/getdata',
-      params: params,
-      method: 'get'
-    }).then(response => {
-      days.push(...Array.from(response.request.response)
-        .map((m, index) => (createDay(index, Number(m))))
-      )
-    }).catch(reason => {
-      days.push(...Array.from(Array(daysInMonth(params.year, params.month)).keys())
-        .map((m, index) => (createDay(index))))
-    }).finally(() => {
-      resolve(days)
-    })
-  })
+  const _days = Array(daysInMonth(params.year, params.month)).keys()
+  const array_days = Array.from(_days)
+  const result = array_days.map((m, index) => (createDay(index)))
+  return result
+  // return new Promise((resolve, reject) => {
+  //   axios({
+  //     url: 'https://isdayoff.ru/api/getdata',
+  //     params: params,
+  //     method: 'get'
+  //   }).then(response => {
+  //     days.push(...Array.from(response.request.response)
+  //       .map((m, index) => (createDay(index, Number(m))))
+  //     )
+  //   }).catch(reason => {
+  //     days.push(...Array.from(Array(daysInMonth(params.year, params.month)).keys())
+  //       .map((m, index) => (createDay(index))))
+  //   }).finally(() => {
+  //     resolve(days)
+  //   })
+  // })
 }
-const weekdays = (
-  [1, 2, 3, 4, 5, 6, 7]
-    .map(d => ({
-      weekday: {
-        short: moment().isoWeekday(d).format('dd'),
-        long: moment().isoWeekday(d).format('dddd')
-      },
-      order: d,
-      day: d % 7
-    })
-))
+
 export {
   createDate,
   daysInMonth,
   prevMonthDays,
   prevMonthWeekLength,
   weekdays,
-  getDays
+  getMonthDays
 }
--- tests/unit/month-days.spec.js
+++ tests/unit/month-days.spec.js
@@ -1,29 +1,21 @@
 import 'jest'
-import { createDate, prevMonthDays, getDays } from '@/common/month-days'
+import { createDate, prevMonthDays, getMonthDays } from '@/common/month-days'
 
-const date = createDate(2022, 8, 1).toDate()
+const date = createDate(2022, 8, 1)
 
 describe('get days', () => {
   // const days = []
   test('В августе 2022 года 31 день', () => {
-    const promise = getDays(date)
-    .then(result => expect(result.length).toBe(31))
-    .catch(reason => Promise.reject(reason))
+    expect(getMonthDays(date).length).toBe(31)
   })
   test('Все элементы массива принадлежат августу', (done)=> {
-    const promise = getDays(date)
-    .then(result => {
-      const _result = result.every(day => day.date.month() === 7)
-      expect(result).toBeTruthy()
-      done()
-    })
+    expect(getMonthDays(date).every(day => day.month === 7)).toBeTruthy()
+    done()
   })
-  const feb2020 = createDate(2020, 2, 1).toDate()
+  const feb2020 = createDate(2020, 2, 1)
   test('В феврале 2020 года 29 дней', (done) => {
-    const promise = getDays(feb2020).then(result => {
-      expect(result.length).toBe(29)
-      done()
-    })
+    expect(feb2020.daysInMonth()).toBe(29)
+    done()
   })
   test('остаток июля на первой неделе августа 2022 года -- 0 дней', () => {
     expect(prevMonthDays(date).length).toBe(0)
@@ -32,6 +24,6 @@ describe('get days', () => {
     expect(prevMonthDays(feb2020).length).toBe(5)
   })
   test('остаток августа на первой неделе сентября 2022 года -- 3 дня', () => {
-    expect(prevMonthDays(createDate(2022, 9, 1).toDate()).length).toBe(3)
+    expect(prevMonthDays(createDate(2022, 9, 1)).length).toBe(3)
   })
 })
