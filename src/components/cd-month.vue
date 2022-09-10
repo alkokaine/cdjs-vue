8873ed3563fad269158dd211a1e9f207b736a12d
Алексей Коковин <KokovinAA@cross-d.ru>
2022-09-10 18:38:59 +0300

    cd-month: revolution

--- src/components/cd-month.vue
+++ src/components/cd-month.vue
@@ -1,24 +1,29 @@
 <template>
-  <cd-list v-loading="isLoading" class="cd-month container mx-auto" :collection="weekdays" key-field="day" 
-    list-class="list-unstyled row justify-content-center"
-    :row-class="['cd-weekday--container col-1 px-1 w-auto']">
+  <cd-list class="cd-month mx-auto container" :class="{ 'compact': compact }" :collection="weekdays" key-field="day" :remote-method="getMonthDays" :resolve-result="resolveDays" :payload="date"
+    list-class="list-unstyled row"
+    :row-class="['col cd-weekday--container px-0', { 'mw-mc': compact, 'w-auto': !compact }]">
     <div slot="header">
       <slot name="month-header"></slot>
     </div>
-    <cd-list v-if="isArray" slot-scope="{ row }" class="mw-mc" :collection="resolvedays(row)" 
-      key-field="day" list-class="list-unstyled col-1 cd-weekdays--wrap mw-mc" row-class="cd-day--wrap d-block mw-mc">
-      <div slot="header" class="fw-bold text-uppercase row">
-        <p class="my-0">
-          <el-checkbox v-if="selectWeekdays" class="el-checkbox" type="checkbox" :checked="isWeekdaySelected(row) || allDaysSelected(row)" @change="weekdayselect({ $event, row }, onWeekdaySelect)"/>
+    <cd-list slot-scope="{ row, index }" :collection="row.days"
+      key-field="number" :list-class="['list-unstyled cd-weekdays--wrap justify-content-center', { 'w-100': !compact}, weekdayClass(row, index)]" :row-class="['cd-day--wrap d-block mx-auto', { 'mw-mc': compact }]">
+      <div slot="header" class="fw-bold text-uppercase mx-auto" :class="{ 'mw-mc': compact }">
+        <p v-if="selectWeekdays" class="my-0 px-0">
+          <el-checkbox :id="checkboxid(row.day)" class="el-checkbox" type="checkbox" :value="isChecked(row)" @change="weekdayselect({ $event, row }, onWeekdaySelect)"/>
         </p>
-        <p class="my-0">
+        <p class="my-0 px-0">
           {{ row.weekday.short }}
         </p>
       </div>
-      <cd-day slot-scope="day" :day="day.row" class="border border-0 mx-auto" :class="{ 'is-selected border-1': isDaySelected(day), 'compact': compact }"
-        @click.native.capture="onDaySelect({ $event, day: day.row })">
+      <cd-day slot-scope="day" :day="day.row" class="mx-auto border-0" :class="{ 'is-selected border-1': isDaySelected(day),  'compact': compact }"
+        @click.native.capture="onDaySelect({ $event, day: day.row, weekday: row })">
+        <p class="my-0" :class="{ 'fw-bold': !compact }" slot="header">
+          {{ day.row.date.date() }}
+        </p>
         <template v-if="!compact">
-          <slot :day="day.row"></slot>
+          <div class="cd-day--content-wrapper px-2">
+            <slot :day="day.row"></slot>
+          </div>
         </template>
       </cd-day>
     </cd-list>
@@ -29,111 +34,105 @@
 </template>
 
 <script>
-import { prevMonthDays, getDays, weekdays } from '@/common/month-days'
+import { createDate, weekdays, prevMonthDays } from '@/common/month-days'
 import CDList from './cd-list.vue'
 import CdDay from './cd-day.vue'
+import { Checkbox } from 'element-ui'
 
 export default {
   name: 'cd-month',
   components: {
     'cd-list': CDList,
-    'cd-day': CdDay
+    'cd-day': CdDay,
+    'el-checkbox': Checkbox
   },
   props: {
-    date: { type: Date, reqiured: true },
+    date: { type: Object, reqiured: true },
     sixDays: { type: Boolean, default: false },
     prependDays: { type: Boolean, default: true },
     compact: { type: Boolean, default: false },
+    pre: { type: Boolean, default: true },
     selectWeekdays: { type: Boolean, default: false },
     onWeekdaySelect: { type: Function },
     onDaySelect: {
       type: Function,
-      default: function ({ $event, day }) {
+      default: function ({ $event, day, weekday }) {
         const calendar = this
-        const currentIndex = calendar.selectedDays.findIndex(i => i.date.date() === day.date.date())
-        if (currentIndex >= 0) calendar.selectedDays.splice(currentIndex, 1)
-        else calendar.selectedDays.push(day)
-        const weekdayIndex = calendar.selectedWeekdays.findIndex(w => w === day.date.day())
-        if (weekdayIndex >= 0) calendar.selectedWeekdays.splice(weekdayIndex, 1)
+        const currentIndex = calendar.selectedDays.findIndex(i => i.date.date() === day.date.date() && i.date.month() === day.date.month())
+        const weekdayIndex = calendar.selectedWeekdays.indexOf(weekday.day)
+        if (weekdayIndex >= 0) {
+          calendar.selectedDays.splice(currentIndex, 1)
+          calendar.selectedWeekdays.splice(weekdayIndex, 1)
+        } else if (currentIndex >= 0) {
+          calendar.selectedDays.splice(currentIndex, 1)
+        } else {
+          calendar.selectedDays.push(day)
+          const filtered = weekday.days.filter(wd => wd.isprev === undefined)
+          const map = filtered.map(wd => calendar.selectedDays.findIndex(d => d.date.date() === wd.date.date()))
+          if (map.every(e => e >= 0)) {
+            calendar.selectedWeekdays.push(weekday.day)
+          }
+        }
       }
     }
   },
   data (calendar) {
     return {
-      weekdays,
+      weekdays: Array,
       isLoading: false,
       selectedWeekdays: [],
-      selectedDays: [],
-      dates: calendar.fetchMonth(calendar.date),
-    }
-  },
-  watch: {
-    date: {
-      handler (newvalue, oldvalue) {
-        const calendar = this
-        if (newvalue !== undefined) {
-          if (oldvalue === undefined || (newvalue.getFullYear() !== oldvalue.getFullYear() || newvalue.getMonth() !== oldvalue.getMonth() )) {
-            calendar.fetchMonth(newvalue)
-          }
-        }
-      }
-    },
-    sixDays: {
-      handler (newvalue) {
-        const calendar = this
-        calendar.fetchMonth(calendar.date)
-      }
+      selectedDays: []
     }
   },
   methods: {
-    fetchMonth (date) {
-      const calendar = this
-      calendar.isLoading = true
-      getDays(date, { pre: 1, covid: 1, sd: (calendar.sixDays === true ? 1 : 0) }).then(response => {
-        calendar.dates = calendar.prependDays ? prevMonthDays(date).concat(response) : response
-        calendar.isLoading = false
-      })
+    getMonthDays (date, resolve) {
+      const days = Array.from(Array(date.daysInMonth()).keys())
+        .map(d => ({ date: createDate(date.year(), date.month() + 1, d + 1) }))
+      if (this.prependDays) {
+        resolve(prevMonthDays(date).concat(days))
+      } else {
+        resolve(days)
+      }
     },
-    weekdayselect({ $event, row}, callback) {
+    resolveDays (days) {
+      this.weekdays = weekdays.map(m => ({
+        days: days.filter(d => d.date.day() === m.day ),
+        weekday: m.weekday,
+        order: m.order,
+        day: m.day
+      }))
+    },
+    weekdayselect({ $event, row }, callback) {
       const calendar = this
       const indexOf =calendar.selectedWeekdays.findIndex(w => w === row.day)
       if (indexOf >= 0) {
         calendar.selectedDays.sort((d1, d2) => d1.date.day() - d2.date.day())
         calendar.selectedWeekdays.splice(indexOf, 1)
-        const indecies = {
-          f: calendar.selectedDays.findIndex(d => d.date.day() === row.day),
-          l: calendar.selectedDays.findLastIndex(d => d.date.day() === row.day)
-        }
-        calendar.selectedDays.splice(indecies.f, (indecies.l - indecies.f + 1))
+        row.days.forEach(day => {
+          const _index = calendar.selectedDays.findIndex(d => d.date.date() === day.date.date())
+          calendar.selectedDays.splice(_index, 1)
+        })
       } else {
         calendar.selectedWeekdays.push(row.day)
-        const newdates = calendar.dates.filter(d => {
-          return d.isprev === undefined && d.date.day() === row.day && calendar.selectedDays.findIndex(s => s.date.date() === d.date.date()) === -1 
+        const filtered = row.days.filter(d => d.isprev === undefined)
+        filtered.forEach(day => {
+          const _index = calendar.selectedDays.findIndex(d => d.date.date() === day.date.date())
+          if (_index < 0) calendar.selectedDays.push(day)
         })
-        const firstIndex = calendar.selectedDays.findIndex(d => d.date.day() === row.day)
-        calendar.selectedDays.splice(firstIndex, 0, ...newdates)
       }
     }
   },
   computed: {
-    resolvedays () {
-      const month = this
+    checkboxid () {
+        return ({ day }) => `wd_${day}`
+    },
+    isChecked () {
+      const calendar = this
       return (row) => {
-        try {
-          return month.dates.filter(d => d.date.day() === row.day)
-        }
-        catch (err) {
-          return []
-        }
+        return calendar.selectedWeekdays.indexOf(row.day) >= 0
       }
     },
-    isArray () {
-      return Array.isArray(this.dates) && typeof this.dates.filter === 'function'
-    },
-    resolvedate () {
-      return (row) => row.date.toDate()
-    },
-    weekdayclass () {
+    weekdayClass () {
       const calendar = this
       return (weekday, index) => {
         const isselected = calendar.selectedWeekdays.indexOf(weekday.day) >= 0
@@ -143,33 +142,7 @@ export default {
     isDaySelected () {
       const calendar = this
       return ({ row }) => {
-        return row.isprev !== true && calendar.selectedDays.findIndex(d => d.date.date() === row.date.date()) >= 0
-      }
-    },
-    selectedDaysOf () {
-      const calendar = this
-      return ({ day }) => (calendar.selectedDays.filter(d => d.date.day() === day)).lenght
-    },
-    daysOfWeekday () {
-      const calendar = this
-      return ({ day }) => {
-        try {
-          return calendar.dates.filter(d => d.isprev === undefined && d.date.day() === day).lenght
-        } catch (err) {
-          return -1
-        }
-      }
-    },
-    isWeekdaySelected () {
-      const calendar = this
-      return ({ day }) => {
-        return calendar.selectedWeekdays.indexOf(day) >= 0
-      }
-    },
-    allDaysSelected () {
-      const calendar = this
-      return ({ day }) => {
-        
+        return calendar.selectedDays.findIndex(d => d.date.date() === row.date.date() && d.date.month() === row.date.month()) >= 0
       }
     }
   }
@@ -179,5 +152,17 @@ export default {
 <style>
   .mw-mc {
     max-width: min-content;
+  }
+  .cd-weekday--container {
+    max-width: 14%;
+  }
+  .holiday {
+    color: red;
+  }
+  .cd-day.compact {
+    max-width: 50px;
+  }
+  .cd-month.compact {
+    width: 300px;
   }
 </style>
