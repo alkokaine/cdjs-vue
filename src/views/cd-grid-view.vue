72d44439758af28cb565cb4827d41251ca303be3
Алексей Коковин <KokovinAA@cross-d.ru>
2022-09-11 08:53:12 +0300

    cd-grid-view

---
+++ src/views/cd-grid-view.vue
@@ -0,0 +1,21 @@
+<template>
+  <cd-grid :collection="collection" :descriptor="columns" key-field="ObjectID"></cd-grid>
+</template>
+
+<script>
+import cdGrid from '@/components/cd-grid.vue'
+import descriptor from '@/assets/descriptors'
+export default {
+  components: { cdGrid },
+  data (view) {
+    return {
+      collection: [descriptor.object, descriptor.object, descriptor.object],
+      columns: descriptor.objectDescriptor
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
