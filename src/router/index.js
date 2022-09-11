c8c17edc62f559087fa7e855d26344d48ce2a8e1
Алексей Коковин <KokovinAA@cross-d.ru>
2022-09-11 08:26:02 +0300

    cd-month-view

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/home-view.vue'
-
+import CDMonthView from '../views/cd-month-view.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,6 +9,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/cd-month',
+    name: 'cd-month',
+    component: CDMonthView
   }
 ]
 
---
+++ src/views/cd-month-view.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cd-month-view">
+    <h3>CD-MONTH</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
