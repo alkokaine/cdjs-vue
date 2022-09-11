58c166a0f80799938dfc33ce6106825d4a74683c
Алексей Коковин <KokovinAA@cross-d.ru>
2022-09-11 09:24:41 +0300

    cd-month-view

--- src/views/cd-month-view.vue
+++ src/views/cd-month-view.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="cd-month-view">
     <h3>CD-MONTH</h3>
+    <cd-month :compact="compact" :select-weekdays="showcheckbox" :date="date"/>
   </div>
 </template>
 
 <script>
+import CDMonth from '@/components/cd-month.vue'
+import { createDate } from '@/common/month-days'
 export default {
-
+  components: {
+    'cd-month': CDMonth
+  },
+  data (view) {
+    return {
+      compact: false,
+      showcheckbox: true,
+      date: createDate(2022, 9, 11)
+    }
+  }
 }
 </script>
 
 <style>
-
+  @import '~bootstrap/dist/css/bootstrap.css';
+  @import '~element-ui/lib/theme-chalk/index.css';
 </style>
