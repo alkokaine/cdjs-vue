7168afbd116917e15ab2ba9efe4d1d4b396921d8
sit19 <alkokaine@yandex.ru>
2022-09-15 22:05:29 +0300

    grid tests

--- tests/unit/cd-grid/decorator.spec.js
+++ tests/unit/cd-grid/decorator.spec.js
@@ -23,6 +23,24 @@ const headCellClassFactory = (headCellClass) => ({
   collection: [],
   headCellClass
 })
+const bodyCellClassFactory = (bodyCell) => ({
+  descriptor: descriptors.objectDescriptor,
+  keyField: 'ObjectID',
+  collection: [descriptors.object, descriptors.object],
+  bodyCellClass: bodyCell
+})
+const rowClassFactory = (rowClass) => ({
+  descriptor: descriptors.objectDescriptor,
+  keyField: 'ObjectID',
+  collection: [descriptors.object, descriptors.object],
+  rowClass
+})
+const bodyCell = jest.fn(({ row, rowindex }, { prop, propindex}) => {
+  return `${prop.datafield}`
+})
+const rowClass = jest.fn(({ row, rowindex }) => {
+  return `row-class-${rowindex}`
+})
 describe('[cd-grid] [head-class] is default or props value', () => {
   it ('[head-class] is undefined', done => {
     const gridwrapper = mount(CDGrid, { propsData: propsDataFactory() })
@@ -98,15 +116,60 @@ describe ('[cd-grid] [body-class] is Array', () => {
 })
 
 describe('[cd-grid][head-cell] class is not default', () => {
-  it ('[cd-grid] has no [.cd-grid-head--cell-base] elements', (done) => {
-    const headCell = jest.fn((property, index) => (property.datafield))
-    const wrapper = mount(CDGrid, { propsData: headCellClassFactory(headCell) })
-    const headerCells = wrapper.findAll('.cd-header--cell')
-    const headercols = wrapper.vm.columns.map(c => wrapper.findAll(`.${c.datafield}`))
+  const headCell = jest.fn((property, index) => (property.datafield))
+  const wrapper = mount(CDGrid, { propsData: headCellClassFactory(headCell) })
+  const headerCells = wrapper.findAll('.cd-header--cell')
+  const propels = wrapper.vm.columns.map(c => wrapper.findAll(`.${c.datafield}`))
+  it ('[cd-grid].[head-cell-class] is function', (done) => { 
+    expect(wrapper.vm.headCellClass).toBe(headCell)
+    expect(typeof wrapper.vm.headCellClass).toBe('function')
+    done()
+  })
+  it (`[cd-grid].[head-cell-class] calls ${wrapper.vm.columns.length} times`, done => {
     expect(headCell.mock.calls.length).toBe(wrapper.vm.columns.length)
+    done()
+  })
+  it (`[cd-grid] has ${wrapper.vm.columns} [.cd-header--cell] elements`, done => {
     expect(headerCells.length).toBe(wrapper.vm.columns.length)
+    done()
+  })
+  it ('[cd-grid] has no default header-cell-class', done => {
     expect(wrapper.findAll('.cd-grid-head--cell-base').length).toBe(0)
-    expect(headercols.every(e => e.wrappers.length === 1)).toBeTruthy()
+    done()
+  })
+  it ('each [cd-grid] property descriptor has one own [head-cell]', done => {
+    expect(propels.every(e => e.wrappers.length === 1)).toBeTruthy()
+    done()
+  })
+})
+describe('[cd-grid][body-cell] class', () => {
+  const wrapper = mount(CDGrid, { propsData: bodyCellClassFactory(bodyCell) })
+  const map = wrapper.vm.columns.map(m => (wrapper.findAll(`.${m.datafield}`)))
+  it ('[cd-grid].[body-cell] is function', done => {
+    expect(typeof wrapper.vm.bodyCellClass).toBe('function')
+    done()
+  })
+  it (`[cd-grid] has ${wrapper.vm.columns.length * wrapper.vm.collection.length} data cells`, (done) => {
+    expect(wrapper.findAll('.cd-grid--cell').length).toBe(wrapper.vm.columns.length * wrapper.vm.collection.length)
+    done()
+  })
+  it (`[cd-grid] calls bodyCellClass function ${wrapper.vm.columns.length * wrapper.vm.collection.length} times`, done => {
+    expect(bodyCell.mock.calls.length).toBe(wrapper.vm.columns.length * wrapper.vm.collection.length)
+    done()
+  })
+  it (`[cd-grid] each body-cell-class repeats ${wrapper.vm.collection.length} times`, done => {
+    expect(map.every(e => e.length === wrapper.vm.collection.length)).toBe(true)
+    done()
+  })
+})
+describe('[cd-grid][row-class] class', () => {
+  const wrapper = mount(CDGrid, { propsData: rowClassFactory(rowClass)})
+  it ('[cd-grid].[row-class] is function', (done) => {
+    expect(typeof wrapper.vm.rowClass).toBe('function')
+    done()
+  })
+  it ('[cd-grid].[row-class] calls collection.length times', (done) => {
+    expect(rowClass.mock.calls.length).toBe(wrapper.vm.collection.length)
     done()
   })
 })
