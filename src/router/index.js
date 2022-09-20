e098706d27dc56c850db91c85da311a28ed0b76d
sit19 <alkokaine@yandex.ru>
2022-09-20 09:37:04 +0300

    props-view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/home-view.vue'
+import CdPropsView from '../views/cd-props-view.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/cd-props',
+    name: 'props',
+    component: CdPropsView
   }
 ]
 
