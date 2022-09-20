50ab0300e3ae8558790ec21ba9382e7dc46cf441
sit19 <alkokaine@yandex.ru>
2022-09-20 10:50:55 +0300

    cd-list-view

--- src/views/cd-list-view.vue
+++ src/views/cd-list-view.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="cd-list-view">
-    <h3>CD-LIST</h3>
+    <cd-list :remote-method="getdata" :payload="payload"
+      :resolve-result="resolveCollection" key-field="key"
+      :collection="collection" row-class="cd-list--test-item">
+      <div slot="header">
+        <h3>CD-LIST</h3>
+        <input type="range" v-model.number="payload"/>
+      </div>
+      <span slot-scope="{ row }">{{ row }}</span>
+    </cd-list>
   </div>
 </template>
 
 <script>
+import CDList from '@/components/cd-list.vue'
 export default {
-
+  components: {
+    'cd-list': CDList
+  },
+  data (view) {
+    return {
+      collection: [],
+      payload: 10
+    }
+  },
+  methods: {
+    resolveCollection (result) {
+      this.collection = result
+    },
+    getdata(payload, resolve) {
+      resolve(Array.from(Array(payload).keys()).map(m => ({ key: m })))
+    }
+  },
+  computed: {
+    rowClass (vm) {
+      return (row, index) => (`cd-list-view--index-${index}-row-${row.key}-payload-${vm.payload}`)
+    }
+  }
 }
 </script>
 
