31512a76ac62dd7111daaa162fcabcd696a86a13
sit19 <alkokaine@yandex.ru>
2022-10-12 09:24:29 +0300

    cd-props test

---
+++ tests/unit/cd-props/decoration.spec.js
@@ -0,0 +1,19 @@
+import CDProps from '@/components/cd-props'
+import { mount } from '@vue/test-utils'
+import descriptor from '@/assets/descriptors'
+import flatterer from '@/common/property-flatter'
+import Vue from 'vue'
+describe('[CD-PROPS] decoration', () => {
+  const propsData2 = {
+    payload: descriptor.object,
+    descriptor: descriptor.objectDescriptor
+  }
+  
+  it ('propClass function was executed 1 times', done => {
+    descriptor.generalPropClass.mockClear()
+    const wrapper = mount (CDProps, { propsData: propsData2 })
+    expect(wrapper.findAll('.cd-custom-property-class').length).toBe(1)
+    expect(descriptor.generalPropClass.mock.calls.length).toBe(1)
+    done()
+  }, 10000)
+})
