b154b06b9687157e43b193e350a1922bd8184ebc
sit19 <alkokaine@yandex.ru>
2022-11-12 15:04:46 +0300

    fieldset: evolution

--- src/components/cd-fieldset.vue
+++ src/components/cd-fieldset.vue
@@ -1,15 +1,15 @@
 <template>
-  <fieldset class="cd-fieldset d-block w-auto position-relative" :class="[{ 'border border-1 has-legend' : hasLegend(owner), 'disabled': disabled }]" :disabled="disabled">
-    <cd-props-base :class="[{ 'mt-2': hasLegend(owner) }]" :descriptor="descriptor" :owner="owner" :payload="payload">
+  <fieldset class="cd-fieldset d-block position-relative" :class="[{ 'border border-1 has-legend' : hasLegend(owner), 'disabled': disabled }]" :disabled="disabled">
+    <cd-props-base :class="[{ 'mt-2': hasLegend(owner) }]" :descriptor="descriptor" :owner="owner" :payload="payload" :config="propConfig">
       <legend v-if="hasLegend(owner)" class="cd-legend position-absolute top-25 start-0 translate-middle-y ms-4" slot="text">
         <p class="cd-legend--text bg-secondary text-white px-2 user-select-none">{{ owner.text }}</p>
       </legend>
-      <template slot-scope="{ property, hasDescriptor }">
-        <cd-fieldset v-if="hasDescriptor" :descriptor="property.descriptor" :owner="property" :payload="payload" :isDisabled="isDisabled">
-          <slot slot-scope="row" :property="row.property" :parent="property" :isDisabled="disabled || row.isDisabled"/>
+      <template slot-scope="{ property, config }">
+        <cd-fieldset v-if="config.hasDescriptor" :descriptor="property.descriptor" :owner="property" :payload="payload" :isDisabled="isDisabled" :field-config="fieldConfig">
+          <slot slot-scope="row" :property="row.property" :parent="property" :value="payload[row.property.datafield]" :config="fieldConfig(row.property)"/>
         </cd-fieldset>
-        <div v-else class="cd-field" :class="resolvePropertyClass(property.propClass)">
-          <slot :property="property" :parent="owner" :isDisabled="disabled || resolveDisabled(property)"/>
+        <div v-else class="cd-field" :class="resolvePropertyClass(property)">
+          <slot :property="property" :parent="owner" :value="payload[property.datafield]" :config="fieldConfig(property)"/>
         </div>
       </template>
     </cd-props-base>
@@ -26,27 +26,29 @@ export default {
     'cd-props-base': CdPropsBase
   },
   data (fieldset) {
+
     return {
-      disabled: fieldset.isDisabled(fieldset.owner)
+      disabled: typeof fieldset.isDisabled === 'function' ? fieldset.isDisabled(fieldset.owner) :fieldset.isDisabled
     }
   },
   props: {
     descriptor: { type: Array, required: true },
     payload: { type: Object, required: true },
     owner: { type: Object, default: Object },
-    isDisabled: { type: Function }
+    isDisabled: { type: Function },
+    fieldConfig: { type: Function, required: true, default: (prop) => ({}) }
   },
   computed: {
     hasLegend () {
       return (property) => decorator.hasLegend(property)
     },
-    resolvePropertyClass (vm) {
-      return (propClass) => typeof propClass === 'function' ? propClass(vm.payload) : propClass
+    propConfig () {
+      return (property) => ({
+        hasDescriptor: decorator.hasDescriptor(property)
+      })
     },
-    resolveDisabled (vm) {
-      return (property) => {
-        return vm.disabled || vm.isDisabled(property)
-      }
+    resolvePropertyClass ({ payload }) {
+      return (property) => decorator.resolvePropertyClass(property, payload)
     }
   }
 }
