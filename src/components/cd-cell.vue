bc74b1f03e4183b9f9904bef8ff0cd4264198ffc
sit19 <alkokaine@yandex.ru>
2022-11-12 15:00:40 +0300

    cell: evolution

--- src/components/cd-cell.vue
+++ src/components/cd-cell.vue
@@ -1,58 +1,61 @@
 <template>
-  <div class="cd-cell" :class="cellClassResolved">
-    <div class="cd-field--input" :class="[{ 'w-100': !input.checkbox, 'cd-checkbox': input.checkbox }]">
-      <template v-if="input.number">
-        <input type="number" :min="property.min" :max="property.max" :value="value"/>
-      </template>
-      <template v-else-if="input.select">
-        <el-select :placeholder="property.placeholder" :value="value">
-          <cd-list class="cd-select--options" :collection="collection" :key-field="property.valuekey" :list-class="['list-unstyled', property.listClass]" rowClass="cd-option">
-            <el-option slot-scope="{ row }" :value="row[property.keyfield]" :label="row[property.labelkey]" :disabled="optionDisabled(row, property.isdisabled)">
-              <template v-if="property.slotdescriptor">
-                <cd-props :descriptor="property.slotdescriptor" :payload="row"></cd-props>
-              </template>
-              <template v-else>
-                {{ row[property.labelkey] }}
-              </template>
-            </el-option>
-          </cd-list>
-        </el-select>
-      </template>
-      <template v-else-if="input.autocomplete">
-        <el-autocomplete :value="value" :fetch-suggestions="fetch"></el-autocomplete>
-      </template>
-      <template v-else-if="input.textarea">
-        <el-input type="textarea" :value="value"/>
-      </template>
-      <template v-else-if="input.checkbox">
-        <el-checkbox :value="value"></el-checkbox>
-      </template>
-      <template v-else-if="input.date">
-        <el-date-picker :value="value"></el-date-picker>
-      </template>
-      <template v-else-if="input.datetime">
-        <el-date-picker type="datetime" :value="value"></el-date-picker>
-      </template>
-      <template v-else-if="input.email">
-        <input type="email" :value="value"/>
-      </template>
-      <template v-else-if="input.radio"></template>
-      <template v-else-if="input.range">
-        <input type="range" :value="value"/>
-      </template>
-      <template v-else-if="input.tel">
-        <input type="tel" :value="value"/>
-      </template>
-      <template v-else-if="input.url">
-        <input type="url" :value="value"/>
-      </template>
-      <template v-else-if="input.file">
-        <input type="file" :value="value"/>
-      </template>
-      <template v-else>
-        <el-input type="text" :value="value"/>
-      </template>
-    </div>
+  <div class="cd-cell" :class="[cellClassResolved]">
+    <template v-if="input.number">
+      <input type="number" class="el-input__inner" :min="property.min" :max="property.max" :value="cellvalue" :disabled="disabled"/>
+    </template>
+    <template v-else-if="input.select">
+      <el-select class="cd-select" :placeholder="property.placeholder" :value="cellvalue" :disabled="disabled">
+        <cd-list class="cd-select--options" :collection="collection" :key-field="property.valuekey" :list-class="['list-unstyled', property.listClass]" rowClass="cd-option">
+          <el-option slot-scope="{ row }" :value="row[property.keyfield]" :label="row[property.labelkey]" :disabled="optionDisabled(row, property.isdisabled)">
+            <template v-if="property.slotdescriptor">
+              <cd-props :descriptor="property.slotdescriptor" :payload="row"></cd-props>
+            </template>
+            <template v-else>
+              {{ row[property.labelkey] }}
+            </template>
+          </el-option>
+        </cd-list>
+      </el-select>
+    </template>
+    <template v-else-if="input.autocomplete">
+      <el-autocomplete class="cd-autocomplete" :placeholder="property.placeholder" :value="cellvalue" :fetch-suggestions="fetch" :disabled="disabled">
+        <div class="cd-autocomplete-item--wrap" slot-scope="{ item }">
+          <cd-props v-if="property.slotdescriptor" class="cd-autocomplete--item" :descriptor="property.slotdescriptor" :payload="item"/>
+          <span v-else>{{ item[property.labelkey] }}</span>
+        </div>
+      </el-autocomplete>
+    </template>
+    <template v-else-if="input.textarea">
+      <el-input class="cd-input" type="textarea" :placeholder="property.placeholder" :value="cellvalue" :disabled="disabled"/>
+    </template>
+    <template v-else-if="input.checkbox">
+      <el-checkbox class="cd-checkbox"  :value="cellvalue" :disabled="disabled"></el-checkbox>
+    </template>
+    <template v-else-if="input.date">
+      <el-date-picker class="cd-date" :placeholder="property.placeholder" :value="cellvalue" :disabled="disabled"></el-date-picker>
+    </template>
+    <template v-else-if="input.datetime">
+      <el-date-picker class="cd-date-time" :placeholder="property.placeholder" type="datetime" :value="cellvalue" :disabled="disabled"></el-date-picker>
+    </template>
+    <template v-else-if="input.email">
+      <input class="el-input__inner" type="email" :placeholder="property.placeholder" :value="cellvalue" :disabled="disabled"/>
+    </template>
+    <template v-else-if="input.radio"></template>
+    <template v-else-if="input.range">
+      <el-slider class="cd-slider" :value="cellvalue" :vertical="property.vertical" :height="property.height" :min="property.min" :max="property.max" :step="property.step" :range="property.range"></el-slider>
+    </template>
+    <template v-else-if="input.tel">
+      <input class="el-input__inner" :placeholder="property.placeholder" type="tel" :value="cellvalue" :disabled="disabled"/>
+    </template>
+    <template v-else-if="input.url">
+      <input class="el-input__inner" :placeholder="property.placeholder" type="url" :value="cellvalue" :disabled="disabled"/>
+    </template>
+    <template v-else-if="input.file">
+      <input class="el-input__inner" type="file" :value="cellvalue" :disabled="disabled"/>
+    </template>
+    <template v-else>
+      <el-input type="text" :placeholder="property.placeholder" :value="value" :disabled="disabled"/>
+    </template>
   </div>
 </template>
 <script>
@@ -60,43 +63,30 @@
   export default {
     name: 'cd-cell',
     props: {
+      input: { type: Object },
       cellClass: { type: [Array, Object, String, Function] },
       property: { type: Object, default: Object },
+      disabled: { type: Boolean },
       value: { type: [Array, String, Number, Boolean, Date] },
       optionDisabled: {
         type: Function,
         default: function (option, isdisabled) {
           if (isdisabled === undefined) return false
           if (typeof isdisabled === 'boolean') return isdisabled
-          if (typeof isdisabled === 'function') {
-            isdisabled(option)
-          }
+          if (typeof isdisabled === 'function') return isdisabled(option)
         }
       },
+      fetchData: { type: Function, default: function (config, query, callback) {
+
+      }}
     },
     components: {
       'cd-list': CDList
     },
     data (cell) {
-      const input = cell.property.input
       return {
         collection: Array,
-        input: {
-          select: input === 'select',
-          autocomplete: input === 'autocomplete',
-          number: ['number', 'money', 'amount'].includes(input),
-          textarea: input === 'textarea',
-          checkbox: input === 'checkbox',
-          date: input === 'date',
-          datetime: input === 'datetime',
-          email: input === 'email',
-          radio: input === 'radio',
-          range: input === 'range',
-          tel: input === 'tel',
-          text: input === 'text' || input === undefined,
-          url: input === 'url',
-          file: input === 'file'
-        }
+        cellvalue: Array.isArray(cell.value) ? Array.from(cell.value) : cell.value
       }
     },
 
@@ -107,7 +97,7 @@
     },
     methods: {
       fetch (query, callback) {
-        callback([])
+        this.fetchData({ config: this.property, query }, callback)
       }
     }
   }
@@ -115,11 +105,33 @@
 <style>
   .el-select-dropdown__item {
     height: unset!important;
-    line-height: unset!important;
+    /* line-height: unset!important; */
   }
   .cd-autocomplete--option {
     white-space: normal;
-    line-height: 1rem;
+    /* line-height: 1rem; */
+  }
+  .el-select-dropdown__empty {
+    font-size: inherit!important;
+  }
+  .cd-select {
+    width: -webkit-fill-available;
+  }
+  .cd-autocomplete {
+    width: -webkit-fill-available;
+  }
+  .cd-date {
+    width: -webkit-fill-available!important;
+  }
+  .cd-date-time {
+    width: -webkit-fill-available!important;
+  }
+  .cd-slider {
+    width: -webkit-fill-available;
+    display: inline-block;
+  }
+  .cd-checkbox {
+    font-size: inherit!important;
   }
 </style>
 <style scoped>
@@ -131,8 +143,5 @@
   /* Firefox */
   input[type=number] {
     -moz-appearance: textfield;
-  }
-  .cd-slider {
-    width: 100%;
   }
 </style>
