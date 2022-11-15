c5009a42760edf3c7f34e1402839c85b318001a6
sit19 <alkokaine@yandex.ru>
2022-11-15 21:04:55 +0300

    search by prefix, show country details

--- src/views/cd-list-filter-view.vue
+++ src/views/cd-list-filter-view.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="cd-list-filter">
     <cd-list :collection="collection" :payload="payload" key-field="code" :resolve-result="resolveResult" :remote-method="getData">
-      <cd-form slot="header" :payload="payload" :descriptor="descriptor" :sync="true">
-        <div slot="header">{{ payload }}</div>
-      </cd-form>
-      <div class="country" slot-scope="{ row }">{{ row }}</div>
+      <cd-form slot="header" :payload="payload" :descriptor="descriptor" :sync="true"></cd-form>
+      <div class="country" slot-scope="{ row }">
+        <span>{{ row }}</span><el-button type="text" size="mini" @click="showDetails(row)">Дополнительно</el-button>
+        <cd-props v-if="isdetails(row)" :payload="details" :descriptor="countrydetails">
+          <div class="country-detail" slot-scope="{ property, value }">
+            <div class="country-flag" v-if="isimage(property)">
+              <img v-if="isimage(property)" :src="value"/>
+            </div>
+            <div v-else>
+              <span class="fw-bold">{{ property.text }}: </span> <span>{{ value }}</span>
+            </div>
+          </div>
+        </cd-props>
+      </div>
       <div slot="footer" v-if="error.code">
         {{ error }}
       </div>
@@ -17,34 +27,107 @@ import adapter from 'axios/lib/adapters/http'
 import fetchData from '@/common/fetch-data'
 import CDForm from '@/components/cd-form'
 import CDList from '@/components/cd-list'
+import CDProps from '@/components/cd-props'
 import AxiosError from 'axios/lib/core/AxiosError'
+
 export default {
   name: 'cd-list-filter',
   components: {
     'cd-list': CDList,
-    'cd-form': CDForm
+    'cd-form': CDForm,
+    'cd-props': CDProps
   },
   data (view) {
     return {
         collection: Array,
         payload: {
-          limit: 10
+          limit: 10,
+          namePrefix: ''
         },
         error: AxiosError,
+        detailserror: AxiosError,
+        details: Object,
         total: Number,
         descriptor: [
           {
             datafield: 'limit',
-            text: 'Кол-во записей',
+            text: 'Кол-во возвращаемых записей',
             input: 'range',
             max: 10
+          },
+          {
+            datafield: 'namePrefix',
+            text: 'Найти страну',
+            placeholder: 'Начните печатать, чтобы найти страну'
+          }
+        ],
+        countrydetails: [
+          {
+            descriptor: [
+              {
+                datafield: 'flagImageUri'
+              },
+              {
+                descriptor: [
+                  {
+                    datafield: 'name',
+                    text: 'Название'
+                  },
+                  {
+                    datafield: 'capital',
+                    text: 'Столица'
+                  }
+                ]
+              },
+              {
+                datafield: 'callingCode',
+                text: 'Телефонный код'
+              },
+              {
+                datafield: 'numRegions',
+                text: 'Количество регионов'
+              },
+              {
+                datafield: 'wikiDataId',
+                text: 'Код вики'
+              }
+            ]
           }
         ]
     }
   },
+  computed: {
+    isdetails ({ details }) {
+      return ({ code }) => code === details.code
+    },
+    isimage () {
+      return ({ datafield }) => datafield === 'flagImageUri'
+    }
+  },
   methods: {
+    showDetails ({ code }) {
+      const list = this
+      fetchData({
+        adapter,
+        url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${String(code).toUpperCase()}`,
+        method: 'get',
+        before (config) {
+          list.detailserror = AxiosError
+          return config
+        },
+        error (reason) {
+          list.detailserror = reason
+        },
+        headers: {
+          'X-RapidAPI-Key': '0d6efbd8a7msh8fcd0fa4c7e36a4p15464ejsn34c8169d4000',
+          'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+      }).then(response => {
+        list.details = response.data.data
+      })
+    },
     getData (payload, resolve) {
-        const list = this
+      const list = this
       fetchData({
         adapter,
         payload,
