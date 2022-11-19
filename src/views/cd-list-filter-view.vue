1e1e4cbc600cc05f2f7cfc9787207065212420b1
sit19 <alkokaine@yandex.ru>
2022-11-19 09:07:21 +0300

    filtered-list example

--- src/views/cd-list-filter-view.vue
+++ src/views/cd-list-filter-view.vue
@@ -1,28 +1,64 @@
 <template>
   <div class="cd-list-filter">
-    <cd-list :collection="collection" :payload="payload" key-field="code" :resolve-result="resolveResult" :remote-method="getData">
+    <cd-list name="countries" :collection="collection" :payload="payload" key-field="code" :resolve-result="resolveResult" :remote-method="getData">
       <cd-form slot="header" :payload="payload" :descriptor="descriptor" :sync="true"></cd-form>
       <div class="country" slot-scope="{ row }">
-        <span>{{ row }}</span><el-button type="text" size="mini" @click="showDetails(row)">Дополнительно</el-button>
-        <cd-props v-if="isdetails(row)" :payload="details" :descriptor="countrydetails">
-          <div class="country-detail" slot-scope="{ property, value }">
-            <div class="country-flag" v-if="isimage(property)">
-              <img v-if="isimage(property)" :src="value"/>
-            </div>
-            <div v-else>
-              <span class="fw-bold">{{ property.text }}: </span> <span>{{ value }}</span>
-            </div>
+        <span>{{ row }}</span><el-button type="text" @click="showCountryDetails(row)">Дополнительно</el-button>
+        <cd-props v-if="isdetails(row)" :payload="country" :descriptor="countrydetails">
+          <div slot-scope="{ property, value }">
+            <label>{{ property.text }}: </label>
+            <template v-if="isregions(property)">
+              <el-button type="text" v-on:click="showRegions">{{ value }}</el-button>
+            </template>
+            <template v-else>
+              <span>{{ value }}</span>
+            </template>
           </div>
         </cd-props>
       </div>
-      <div slot="footer" v-if="error.code">
-        {{ error }}
+      <div slot="footer">
+        <el-pagination :current-page="countryPage" :total="total" :page-size="payload.limit" v-on:current-change="onPageChange($event, payload)" :layout="layout"></el-pagination>
       </div>
-    </cd-list>
+    </cd-list> 
+    <el-dialog width="80%" :visible.sync="showDialog" :close-on-click-modal="false" @close="ondialogclose">
+      <div slot="title" class="h3">
+        <p>Всего регионов {{ country.numRegions }}</p>
+      </div>
+      <cd-form name="regions-filter" :payload="regionPayload" :descriptor="descriptor" :sync="true">
+        <cd-list slot="footer" v-if="showDialog" name="regions" :collection="regions" key-field="wikiDataId" :payload="regionPayload" :resolve-result="resolveRegions" :remote-method="getRegions" list-class="row list-unstyled" :row-class="resolveRegionClass">
+          <div slot-scope="{ row }">
+            <div @click="getRegionDetails($event, row)">{{ row.name }}</div>
+            <cd-props v-if="isregiondetails(row)" class="container-sm text-start border" :payload="region" :descriptor="regiondetails">
+              <div slot-scope="{ property, value }">
+                <label class="cd-label">{{ property.datafield }}:</label> 
+                <template v-if="isCities(property)">
+                  <el-button type="text" @click="showCities($event, row)">{{ value }}</el-button>
+                </template>
+                <template v-else>
+                  <span>{{ value }}</span>
+                </template>
+              </div>
+              <div v-if="showCityList" class="region-cities" slot="content">
+                <cd-list key-field="id" name="cities" :remote-method="getCities" :payload="cityPayload" :resolve-result="resolveCities" :collection="cities">
+                  <cd-form slot="header" :payload="cityPayload" :descriptor="descriptor" :sync="true">
+                    <el-pagination slot="footer" :current-page="cityPage" :total="totalcities" :layout="layout" :page-size="cityPayload.limit" v-on:current-change="onPageChange($event, cityPayload)"></el-pagination>
+                  </cd-form>
+                  <span slot-scope="{ row }">{{ row }}</span>
+                </cd-list>
+              </div>
+            </cd-props>
+          </div>
+          <div slot="footer">
+            <el-pagination :current-page="currentPage" :total="country.numRegions" :layout="layout" :page-size="regionPayload.limit" v-on:current-change="onPageChange($event, regionPayload)"></el-pagination>
+          </div>
+        </cd-list>
+      </cd-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import keys from '../../keys'
 import adapter from 'axios/lib/adapters/http'
 import fetchData from '@/common/fetch-data'
 import CDForm from '@/components/cd-form'
@@ -37,18 +73,11 @@ export default {
     'cd-form': CDForm,
     'cd-props': CDProps
   },
-  data (view) {
-    return {
-        collection: Array,
-        payload: {
-          limit: 10,
-          namePrefix: ''
-        },
-        error: AxiosError,
-        detailserror: AxiosError,
-        details: Object,
-        total: Number,
-        descriptor: [
+  props: {
+    descriptor: { 
+      type: Array, 
+      default: function () {
+        return [
           {
             datafield: 'limit',
             text: 'Кол-во возвращаемых записей',
@@ -60,56 +89,189 @@ export default {
             text: 'Найти страну',
             placeholder: 'Начните печатать, чтобы найти страну'
           }
-        ],
-        countrydetails: [
-          {
-            descriptor: [
-              {
-                datafield: 'flagImageUri'
-              },
-              {
-                descriptor: [
-                  {
-                    datafield: 'name',
-                    text: 'Название'
-                  },
-                  {
-                    datafield: 'capital',
-                    text: 'Столица'
-                  }
-                ]
-              },
-              {
-                datafield: 'callingCode',
-                text: 'Телефонный код'
-              },
-              {
-                datafield: 'numRegions',
-                text: 'Количество регионов'
-              },
-              {
-                datafield: 'wikiDataId',
-                text: 'Код вики'
-              }
-            ]
-          }
         ]
+      }
+    }
+  },
+  data (view) {
+    return {
+      collection: Array,
+      currentPage: 1,
+      countryPage: 1,
+      showDialog: false,
+      layout: 'prev, pager, next, total',
+      regions: [],
+      cities: [],
+      cityPage: 1,
+      totalcities: 0,
+      payload: {
+        limit: 1,
+        namePrefix: '',
+        offset: 0
+      },
+      regionPayload: {
+        limit: 1,
+        offset: 0,
+        namePrefix: ''
+      },
+      cityPayload: {
+        limit: 1,
+        offset: 0,
+        namePrefix: ''
+      },
+      showCityList: false,
+      country: Object,
+      region: Object,
+      city: Object,
+      error: AxiosError,
+      totalregions: null,
+      total: 0,
+      countrydetails: [
+        {
+          descriptor: [
+            {
+              datafield: 'flagImageUri'
+            },
+            {
+              descriptor: [
+                {
+                  datafield: 'name',
+                  text: 'Название'
+                },
+                {
+                  datafield: 'capital',
+                  text: 'Столица'
+                }
+              ]
+            },
+            {
+              datafield: 'callingCode',
+              text: 'Телефонный код'
+            },
+            {
+              datafield: 'numRegions',
+              text: 'Количество регионов'
+            },
+            {
+              datafield: 'wikiDataId',
+              text: 'Код вики'
+            }
+          ]
+        }
+      ],
+      regiondetails: [
+            {
+              descriptor: [
+                {
+                  datafield: 'capital',
+                  text: 'capital',
+                },
+                {
+                  datafield: 'isoCode',
+                  text: 'isoCode',
+                },
+                {
+                  datafield: 'countryCode',
+                  text: 'countryCode',
+                },
+              ],
+              propClass: 'row row-cols-3'
+            },
+            {
+              descriptor: [
+                {
+                  datafield: 'numCities',
+                  text: 'numCities',
+                },
+                {
+                  datafield: 'wikiDataId',
+                  text: 'wikiDataId',
+                },
+                {
+                  datafield: 'fipsCode',
+                  text: 'fipsCode',
+                },
+              ],
+              propClass: 'row row-cols-3'
+            }
+          ]
     }
   },
   computed: {
-    isdetails ({ details }) {
-      return ({ code }) => code === details.code
+    resolveRegionClass ({ region }) {
+      return ({ isoCode }) => (region.isoCode === isoCode ? 'pointer d-inline-block bg-white text-dark' : 'pointer w-33 mx-auto')
+    },
+    isregiondetails ({ region }) {
+      return ({ isoCode }) => isoCode === region.isoCode
+    },
+    isdetails ({ country }) {
+      return ({ code }) => code === country.code
+    },
+    isCities () {
+      return ({ datafield }) => datafield === 'numCities'
     },
     isimage () {
       return ({ datafield }) => datafield === 'flagImageUri'
+    },
+    isregions () {
+      return ({ datafield }) => datafield === 'numRegions'
+    },
+    getRegions ({ country }) { 
+      return ((payload, resolve) => {
+        fetchData({
+          adapter,
+          url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${country.code}/regions`,
+          method: 'get',
+          payload,
+          headers: keys.geokeys
+        }).then(response => {
+          resolve(response.data)
+        })
+      })
+    },
+    getCities ({ region }) {
+      return (payload, resolve) => {
+        setTimeout(() => fetchData({
+          adapter,
+          url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${region.countryCode}/regions/${region.isoCode}/cities`,
+          method: 'get',
+          payload,
+          headers: keys.geokeys
+        }).then(response => {
+          resolve(response.data)
+        }), 3000)
+      }
     }
   },
   methods: {
-    showDetails ({ code }) {
+    showRegions (event) {
+      this.showDialog = true
+    },
+    ondialogclose () {
+      this.showDialog = false
+      this.regions = Array
+      this.regionPayload.limit = 1
+      this.cities = Array
+    },
+    getRegionDetails(event, row) {
       const list = this
       fetchData({
         adapter,
-        url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${String(code).toUpperCase()}`,
+        url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${row.countryCode}/regions/${row.isoCode}`,
+        method: 'get',
+        headers: keys.geokeys
+      }).then(response => {
+        list.region = response.data.data
+      })
+    },
+    showCities () {
+      this.showCityList = true
+    },
+    showCountryDetails ({ code }) {
+      const list = this
+      fetchData({
+        adapter,
+        url: `https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${code}`,
         method: 'get',
         before (config) {
           list.detailserror = AxiosError
@@ -118,12 +280,9 @@ export default {
         error (reason) {
           list.detailserror = reason
         },
-        headers: {
-          'X-RapidAPI-Key': '0d6efbd8a7msh8fcd0fa4c7e36a4p15464ejsn34c8169d4000',
-          'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-        }
+        headers: keys.geokeys
       }).then(response => {
-        list.details = response.data.data
+        list.country = response.data.data
       })
     },
     getData (payload, resolve) {
@@ -141,17 +300,25 @@ export default {
         },
         timeout:5000,
         url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/countries',
-        headers: {
-            'X-RapidAPI-Key': '0d6efbd8a7msh8fcd0fa4c7e36a4p15464ejsn34c8169d4000',
-            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-          }
+        headers: keys.geokeys
       }).then(response => { 
-        list.total = response.data.metadata.totalCount
-        resolve(response.data.data)
+        resolve(response.data)
       })
     },
-    resolveResult (response) {
-      this.collection = response
+    resolveResult ({ data, metadata }) {
+      this.collection = data
+      this.total = metadata.totalCount
+    },
+    resolveRegions ({ data, metadata }) {
+      this.regions = data
+      this.totalregions = metadata.totalCount
+    },
+    resolveCities ({ data, metadata }) {
+      this.cities = data
+      this.totalcities = metadata.totalCount
+    },
+    onPageChange (event, payload) {
+      payload.offset = payload.limit * (event - 1)
     }
   }
 }
@@ -164,5 +331,11 @@ export default {
   }
   .country-flag {
     width: 150px;
+  }
+  .w-33 {
+    width: 30%;
+  }
+  .pointer {
+    cursor: pointer;
   }
 </style>
