cb6e599d1c1c1cf60e69dbfe56184e5538ed8ea8
sit19 <alkokaine@yandex.ru>
2022-11-20 23:44:13 +0300

    form v-model view

---
+++ src/views/cd-form-model-view.vue
@@ -0,0 +1,31 @@
+<template>
+  <cd-form class="row" form-class="col" :payload="formModel" :descriptor="inputs" :sync="true">
+    <code class="col" slot="footer" slot-scope="{ payload }">
+      {{ payload }}
+    </code>
+  </cd-form>
+</template>
+
+<script>
+import CDForm from '@/components/cd-form.vue'
+import descriptors from '@/assets/descriptors'
+export default {
+  name: 'cd-form-v-model',
+  components: {
+    'cd-form': CDForm
+  },
+  data (vmodel) {
+    return {
+      formModel: descriptors.inputObject,
+      inputs: descriptors.inputs,
+    }
+  }
+}
+</script>
+
+<style>
+  code {
+    white-space: pre;
+    text-align: start;
+  }
+</style>
