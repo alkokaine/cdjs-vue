22c68fb9d10e7892725844bad8153c40ac2e960d
sit19 <alkokaine@yandex.ru>
2022-11-22 06:36:35 +0300

    simple recursive list

---
+++ src/components/cd-rec-list.vue
@@ -0,0 +1,41 @@
+<template>
+  <cd-list :collection="collection" :key-field="keyField" list-class="list-unstyled row" row-class="col">
+    <template slot-scope="{ row }">
+      <cd-rec-list v-if="row[recursive]" :collection="row[recursive]" :key-field="keyField" :recursive="recursive">
+        <template slot-scope="{ li }">
+          <slot :li="li"></slot>
+        </template>
+      </cd-rec-list>
+      <template v-else>
+        <slot :li="row"></slot>
+      </template>
+    </template>
+  </cd-list>
+</template>
+
+<script>
+import CDList from '@/components/cd-list.vue'
+export default {
+  name: 'cd-rec-list',
+  components: {
+    'cd-list': CDList
+  },
+  props: {
+    collection: { type: Array },
+    recursive: { type: String },
+    keyField: { type: String }
+  },
+  data (list) {
+    return {}
+  }
+}
+</script>
+
+<style>
+  .w-15 {
+    width: 15%;
+  }
+  .container-fluid > li {
+    max-width: min-content;
+  }
+</style>
