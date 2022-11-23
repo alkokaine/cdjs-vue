4948aa66ae632f44b5abff8a0942f46fc3b4fe71
sit19 <alkokaine@yandex.ru>
2022-11-23 06:54:10 +0300

    month view

--- src/views/cd-month-view.vue
+++ src/views/cd-month-view.vue
@@ -2,25 +2,87 @@
   <div class="cd-month-view">
     <h3>CD-MONTH</h3>
     <cd-month :compact="compact" :select-weekdays="showcheckbox" :date="date">
-      <div class="day-content" slot-scope="{ day }">
-        {{ day.date }}
-      </div>
+      <cd-list slot-scope="{ day }" :collection="resolveMatches(day)" key-field="_id" list-class="list-unstyled">
+        <div slot-scope="{ row }" class="row justify-content-center">
+          <div class="col home-team mw-50">
+            <div class="w-auto">
+              <span class="d-inline-block w-100"></span>
+              <img :src="row.event.home_flag" />
+            </div>
+          </div>
+          <div class="col away-team mw-50">
+            <div class="w-auto">
+              <span class="d-inline-block w-100"></span>
+              <img :src="row.event.away_flag"/>
+            </div>
+          </div>
+        </div>
+      </cd-list>
     </cd-month>
   </div>
 </template>
 
 <script>
+import CDList from '@/components/cd-list.vue'
 import CDMonth from '@/components/cd-month.vue'
+import adapter from 'axios/lib/adapters/http'
 import { createDate } from '@/common/month-days'
+import keys from '@/../keys'
+import fetchData from '@/common/fetch-data'
+import moment from 'moment'
+import Vue from 'vue'
 export default {
   components: {
-    'cd-month': CDMonth
+    'cd-month': CDMonth,
+    'cd-list': CDList
   },
   data (view) {
     return {
       compact: false,
       showcheckbox: true,
-      date: createDate(2022, 9, 11)
+      date: createDate(2022, 11, 1),
+      matches: Array
+    }
+  },
+  computed: {
+    resolveMatches ({ matches }) {
+      return ({ date }) => {
+        if (matches.filter === undefined) {
+          return []
+        }
+        else {
+          const res_ = matches.filter(m =>
+            m.moment.date() === date.date() &&
+            m.moment.month() === date.month() &&
+            m.moment.year() === date.year()
+          )
+          setTimeout(() => console.log(res_), 300)
+          return res_
+        }
+      }
+    }
+  },
+  watch: {
+    date: {
+      immediate: true,
+      handler (newvalue) {
+        const view = this
+        fetchData({
+          method: 'get',
+          error (reason) {
+            // list.error = reason
+            console.error(reason)
+          },
+          timeout:5000,
+          url: '/qatar2022/match',
+          headers: keys.qatar2022
+        }).then(response => {
+          view.matches = response.data.data.map(m => ({
+            moment: moment(new Date(m.local_date)),
+            event: m
+          }))
+        })
+      }
     }
   }
 }
@@ -29,4 +91,12 @@ export default {
 <style>
   @import '~bootstrap/dist/css/bootstrap.css';
   @import '~element-ui/lib/theme-chalk/index.css';
+
+  .mw-50 {
+    max-width: 75px;
+  }
+  img {
+    max-width: 95%;
+    max-height: auto;
+  }
 </style>
