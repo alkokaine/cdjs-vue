8276203423f248295af91aeaae930a559c33389b
Алексей Коковин <KokovinAA@cross-d.ru>
2022-11-29 14:48:32 +0300

    month settings form

--- src/views/cd-month-view.vue
+++ src/views/cd-month-view.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="cd-month-view">
-    <cd-month :select-weekdays="showcheckbox" :date="date" :schedule="matches" :compareDate="compareDate">
-      <h3 slot="month-header">CD-MONTH
-        <el-date-picker v-model="mdate"></el-date-picker>
-      </h3>
-      <cd-list slot-scope="{ events }" :collection="events" key-field="_id" class="py-2" list-class="list-unstyled" row-class="match-short py-2 mx-2">
+    <cd-month :select-weekdays="settings.selectWeekdays" :date="date" :schedule="matches" :compareDate="compareDate">
+      <cd-form slot="month-header" :payload="settings" :descriptor="descriptor" :sync="true">
+        <h3 slot="header">CD-MONTH</h3>
+      </cd-form>
+      <cd-list slot-scope="{ events }" :collection="events" key-field="_id" class="py-2" list-class="list-unstyled my-0" row-class="match-short py-2 mx-2">
         <div slot-scope="{ row }" class="row justify-content-center">
           <div class="col home-team mw-50">
             <div class="w-auto team-flag border border-1">
@@ -30,6 +30,7 @@
 <script>
 import CDList from '@/components/cd-list.vue'
 import CDMonth from '@/components/cd-month.vue'
+import CDForm from '@/components/cd-form.vue'
 import { createDate } from '@/common/month-days'
 import keys from '@/../keys'
 import fetchData from '@/common/fetch-data'
@@ -38,14 +39,34 @@ import moment from 'moment'
 export default {
   components: {
     'cd-month': CDMonth,
-    'cd-list': CDList
+    'cd-list': CDList,
+    'cd-form': CDForm
   },
   data (view) {
     return {
-      compact: false,
-      showcheckbox: true,
-      mdate: new Date(Date.now()),
-      matches: []
+      matches: [],
+      settings: {
+        compact: false,
+        selectWeekdays: true,
+        mdate: new Date(Date.now())
+      },
+      descriptor: [
+        {
+          datafield: 'compact',
+          text: 'Компактный режим',
+          input: 'checkbox'
+        },
+        {
+          datafield: 'selectWeekdays',
+          text: 'Чекбоксы у дней недели',
+          input: 'checkbox'
+        },
+        {
+          datafield: 'mdate',
+          text: 'Дата календаря',
+          input: 'date'
+        }
+      ]
     }
   },
   methods: {
@@ -56,8 +77,8 @@ export default {
     }
   },
   computed: {
-    date ({ mdate }) {
-      return createDate(mdate.getFullYear(), mdate.getMonth()+1, mdate.getDate())
+    date ({ settings }) {
+      return createDate(settings.mdate.getFullYear(), settings.mdate.getMonth()+1, settings.mdate.getDate())
     }
   },
   watch: {
