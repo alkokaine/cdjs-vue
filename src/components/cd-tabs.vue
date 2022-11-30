424a37c0b5e9268d402a27f89f81406633a2880e
Алексей Коковин <KokovinAA@cross-d.ru>
2022-11-30 14:37:56 +0300

    tabs-list location

--- src/components/cd-tabs.vue
+++ src/components/cd-tabs.vue
@@ -1,11 +1,18 @@
 <template>
-  <cd-list class="cd-tabs" :collection="tabs" :key-field="tabKey" list-class="cd-tabs--wrap" row-class="cd-tab--wrap" list-role="tablist" item-role="tab">
+  <cd-list class="cd-tabs" :collection="tabs" :key-field="tabKey" :class="[{ 'col': isRow, 'row flex-nowrap': isCol }]" :list-class="['cd-tabs--wrap list-unstyled', { 'row': isRow, 'col': isCol }]" :row-class="['cd-tab--wrap', { 'col': isRow, 'row': isCol } ]" list-role="tablist" item-role="tab">
+    <template v-if="inFooter || inRight" slot="header">
+      <div class="cd-tabs-content">
+        <slot name="content"></slot>
+      </div>
+    </template>
     <div class="cd-tab" slot-scope="{ row, index }" :class="[row.class, { 'is-disabled pe-none': resolveTabDisabled(row, index) }]">
       <slot :tab="row" :index="index"></slot>
     </div>
-    <div class="cd-tabs--content" slot="footer">
-      <slot name="content"></slot>
-    </div>
+    <template v-if="inHeader || inLeft" slot="footer">
+      <div class="cd-tabs--content">
+        <slot name="content"></slot>
+      </div>
+    </template>
   </cd-list>
 </template>
 
@@ -20,7 +27,15 @@ export default {
     tabs: { type: Array, required: true, description: 'Вкладки' },
     tabCaption: { type: String, description: 'Свойство объекта из массива [tabs], в котором находится текст вкладки' },
     tabKey: { type: String, required: true, default: 'key', description: 'Свойство объекта из массива [tabs], в котором находится идентификатор вкладки' },
-    isTabDisabled: { type: Function, description: 'Вычисляем для объекта вкладки и её индекса в массиве [tabs], будет ли вкладка задисаблена' }
+    isTabDisabled: { type: Function, description: 'Вычисляем для объекта вкладки и её индекса в массиве [tabs], будет ли вкладка задисаблена' },
+    orientation: { 
+      type: String, 
+      default: 'row-top', 
+      validator: function (value) {
+        return ['row-top', 'row-bottom', 'col-left', 'col-right'].indexOf(value) >= 0
+      },
+      description: 'Расположение списка вкладок'
+    }
   },
   computed: {
     resolveTabDisabled () {
@@ -31,11 +46,40 @@ export default {
       : isFunction 
         ? isTabDisabled(row, index) 
         : isTabDisabled
+    },
+    inHeader ({ orientation }) {
+      return ['row-top'].indexOf(orientation) >= 0
+    },
+    inFooter ({ orientation }) {
+      return ['row-bottom'].indexOf(orientation) >= 0
+    },
+    isRow ({ orientation }) {
+      return ['row-bottom', 'row-top'].indexOf(orientation) >= 0
+    },
+    isCol ({ orientation }) {
+      return ['col-left', 'col-right'].indexOf(orientation) >= 0
+    },
+    inLeft ({ orientation }) {
+      return ['col-left'].indexOf(orientation) >= 0
+    },
+    inRight ({ orientation }) {
+      return ['col-right'].indexOf(orientation) >= 0
     }
   }
 }
 </script>
 
 <style>
-
+  .cd-tabs--content {
+    padding: 1em;
+  }
+  .cd-tab--wrap {
+    max-width: max-content;
+  }
+  .cd-tabs--wrap {
+    max-width: max-content;
+  }
+  .cd-tab {
+    min-width: max-content;
+  }
 </style>
--- src/views/cd-tabs-view.vue
+++ src/views/cd-tabs-view.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="cd-tabs-view">
-    <cd-tabs :tabs="tabs" tab-key="key"></cd-tabs>
-  </div>
+  <cd-prop-editor class="container-sm" :payload="settings" :descriptor="descriptor">
+    <cd-tabs class="container-sm" :tabs="tabs" tab-key="key" :orientation="settings.orientation">
+      <span slot-scope="{ tab }" v-on:click.capture="setcurrent($event, tab)">{{ tab }}</span>
+      <div slot="content">
+        {{ current }}
+      </div>
+    </cd-tabs>    
+  </cd-prop-editor>
 </template>
 
 <script>
 import CDTabs from '@/components/cd-tabs.vue'
+import CDPropEditor from '@/components/cd-prop-editor'
 export default {
   components:{
-    'cd-tabs': CDTabs
+    'cd-tabs': CDTabs,
+    'cd-prop-editor': CDPropEditor
   },
   data (tabs) {
     return {
-      tabs: [ { key: 1, tab: 1}, { key: 2, tab: 2}, { key: 3, tab: 3} ]
+      tabs: [ { key: 1, tab: 1}, { key: 2, tab: 2}, { key: 3, tab: 3} ],
+      current: Object,
+      settings: {
+        orientation: 'row-top'
+      },
+      descriptor: [
+        {
+          datafield: 'orientation',
+          text: 'ориентация',
+          input: 'select',
+          labelkey: 'label',
+          valuekey: 'value',
+          values: [
+            {
+              label: 'row-top',
+              value: 'row-top'
+            },
+            {
+              label: 'row-bottom',
+              value: 'row-bottom'
+            },
+            {
+              label: 'col-left',
+              value: 'col-left'
+            },
+            {
+              label: 'col-right',
+              value: 'col-right'
+            }
+          ]
+        },
+      ]
+    }
+  },
+  methods: {
+    setcurrent (event, tab) {
+      this.current = structuredClone(tab)
     }
   }
 }
