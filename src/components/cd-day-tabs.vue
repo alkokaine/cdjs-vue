70c5b6d704baea27b71edb57da6b98df8befcb15
Алексей Коковин <KokovinAA@cross-d.ru>
2022-11-30 10:24:19 +0300

    cd-day-tabs & tests

---
+++ src/components/cd-day-tabs.vue
@@ -0,0 +1,28 @@
+<template>
+  <cd-tabs class="cd-day-tabs" :tabs="days">
+    <div slot-scope="{ tab }">{{ tab }}</div>
+    <cd-day slot="content" :day="currentDay"></cd-day>
+  </cd-tabs>
+</template>
+
+<script>
+import CDTabs from '@/components/cd-tabs.vue'
+export default {
+  name: 'cd-day-tabs',
+  components: {
+    'cd-tabs': CDTabs
+  },
+  props: {
+    days: { type: Array, required: true, description: 'Коллекция дней' }
+  },
+  data (tabs) {
+    return {
+      currentDay: Object
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
