2849f835af0af6aacd44afd453e35e3155e92422
Алексей Коковин <KokovinAA@cross-d.ru>
2022-12-04 19:09:10 +0300

    day-tabs evolution

--- src/components/cd-day-tabs.vue
+++ src/components/cd-day-tabs.vue
@@ -1,10 +1,19 @@
 <template>
-  <cd-tabs class="cd-day-tabs container" :tabs="days" tab-key="daykey" :orientation="orientation">
-    <cd-day class="cd-day--wrap cd-day--tab p-2 border border-1" :class="[{ 'mw-100': isCol }]" slot-scope="{ tab }" :day="tab" v-on:click.native="selectDay($event, tab)">
-      <span slot="header">{{ tab.date }}</span>
+  <cd-tabs class="cd-day-tabs container" :tabs="days" tab-key="daykey" 
+    :orientation="orientation" :tab-list-class="{ 'align-items-end': inLeft, 'align-items-start': inRight }" 
+    :on-tab-select="selectDay">
+    <cd-day class="cd-day--wrap cd-day--tab" :class="[{ 'mw-100': isCol }]" slot-scope="{ tab }" :day="tab">
+      <span slot="header">{{ tabCaption(tab) }}</span>
     </cd-day>
     <div slot="content" class="p-2 m-2 w-auto">
-      <slot :content="selectedDays"></slot>
+      <cd-list :collection="selectedDays" key-field="daykey" list-class="list-unstyled" row-class="month-day--details row w-auto">
+        <cd-day slot-scope="{ row }" :day="row">
+          <div class="cd-day--details fw-bold" slot="header">
+            {{ tabCaption(row) }}
+          </div>
+          <slot :day="row"></slot>
+        </cd-day>
+      </cd-list>
     </div>
   </cd-tabs>
 </template>
@@ -12,40 +21,27 @@
 <script>
 import CDTabs from '@/components/cd-tabs.vue'
 import CDDay from '@/components/cd-day.vue'
-
-const defaultSelectedDays = []
-
+import CDList from '@/components/cd-list.vue'
+const formatter = Intl.DateTimeFormat('ru-RU')
 export default {
   name: 'cd-day-tabs',
   components: {
     'cd-tabs': CDTabs,
-    'cd-day': CDDay
+    'cd-day': CDDay,
+    'cd-list': CDList
   },
   props: {
     days: { type: Array, required: true, description: 'Коллекция дней' },
     orientation: { type: String, description: 'Положение и направление дней'},
     compareDate: { type: Function, required: true, description: 'Функция сравнения дат' },
     multiple: { type: Boolean, default: false, description: 'Можно ли выбрать несколько дней' },
+    format: { type: String, default: 'dd-MM-YYYY', description: 'Формат заголовка вкладки' },
     selectDay: { 
       type: Function, 
-      default: function (event, tab) {
-        const daytabs = this
-        const fi = defaultSelectedDays.findIndex(s => s === tab.daykey)
-        if (fi >= 0) defaultSelectedDays.splice(fi, 1)
-        else if (daytabs.multiple) {
-          defaultSelectedDays.push(tab.daykey)
-        } else {
-          defaultSelectedDays.pop()
-          defaultSelectedDays.push(tab.daykey)
-        }
-      },
       description: 'Функция, которая выполняется по клику на день'
     },
     selectedDays: {
       type: Array, 
-      default: function () {
-        return defaultSelectedDays
-      },
       description: 'Массив выбранных дат'
     }
   },
@@ -57,7 +53,17 @@ export default {
     isCol ({ orientation }) {
       return ['col-left', 'col-right'].indexOf(orientation) >= 0
     },
-  }
+    inLeft ({ orientation }) {
+      return orientation === 'col-left'
+    },
+    inRight ({ orientation }) {
+      return orientation === 'col-right'
+    },
+    tabCaption ({ format }) {
+      return ({ date }) => formatter.format(date, format)
+    }
+  },
+  
 }
 </script>
 
