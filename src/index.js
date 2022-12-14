0671a7ecdfeb060791125593a4ae13bb5a851acb
Алексей Коковин <KokovinAA@cross-d.ru>
2022-12-14 21:02:10 +0300

    cdjs index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import CDForm from '@/components/cd-form'
 import CDGrid from '@/components/cd-grid'
 import CDMonth from '@/components/cd-month'
 import CDPropEditor from '@/components/cd-prop-editor'
+import CDRecList from '@/components/cd-rec-list'
 export default {
     CDList,
     CDTabs,
     CDForm,
     CDGrid,
     CDMonth,
-    CDPropEditor
+    CDPropEditor,
+    CDRecList
 }
