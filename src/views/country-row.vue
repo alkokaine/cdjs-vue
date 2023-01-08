7c7eaf25debebda79e75c9401cb3babb3fe484be
sit19 <alkokaine@yandex.ru>
2023-01-08 10:08:59 +0300

    country, currency

---
+++ src/views/country-row.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="country-row">
+    <span>{{ country }}</span>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'country-row',
+  props: {
+    country: { type: Object, required: true },
+  }
+}
+</script>
+
+<style>
+
+</style>
