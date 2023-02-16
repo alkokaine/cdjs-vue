ced1ff1f098c09fa9150cb02c10801fed361ac6d
Alexey Kokovin <Alexey.Kokovin@yandex.ru>
2023-02-16 20:49:00 +0300

    example evolution

--- src/views/cd-list-file-mil.vue
+++ src/views/cd-list-file-mil.vue
@@ -1,14 +1,15 @@
 <template>
-  <cd-list-base class="container" :get="url" :collection="loaded" key-field="href" list-class="list-unstyled">
+  <cd-list-base class="container" :get="url" :collection="pagesLoaded" key-field="href" list-class="list-unstyled">
     <input slot="header" v-model.lazy="url">
-    <cd-get-list slot-scope="{ row }" :get="row.href" :collection="row.links" key-field="href" list-class="list-unstyled row row-cols-6" row-class="border border-1 m-3 p-3" :resolve-result="resolveLinks" :on-error="onError" :error="error">
-      <div :id="row.innerText" slot="header">{{ row.href }}</div>
+    <cd-get-list slot-scope="{ row }" :get="row.href" :collection="row.links" key-field="href" list-class="list-unstyled row row-cols-6" 
+      row-class="border border-1 m-3 p-3" :resolve-result="resolveLinks" :on-error="onError" :error="error">
+      <div :id="row.innerText" slot="header">Страница {{ row.innerText }}</div>
       <notice-file class="my-auto" slot-scope="{ row }" :file="row"></notice-file>  
     </cd-get-list>
-    <cd-list-base slot="footer" :collection="pages" key-field="href" list-class="list-unstyled row">
+    <cd-list-base slot="footer" :collection="pages" class="position-fixed bottom-0 border border-1 mt-2 bg-light" key-field="href" list-class="list-unstyled row" row-class="w-auto">
       <template slot-scope="{ row }">
         <template v-if="isLoaded(row)">
-          <a :href="rowHref(row)">{{ row.innerHTML }}</a>
+          <a class="btn btn-link btn-sm" :href="rowHref(row)">{{ row.innerHTML }}</a>
         </template>
         <template v-else>
           <button class="btn btn-link btn-sm" v-on:click="getPage($event, { row })">
@@ -28,6 +29,8 @@ import CDGetList from '@/components/cd-get-list'
 import CDListBase from '@/components/cd-list-base.vue'
 import NoticeFile from './notice-file.vue'
 import { AxiosError } from 'axios'
+const replaceMil = (text) => (text.replace('http://', 'https://').replace('https://structure.mil.ru', '/mil'))
+
 const domParser = new DOMParser()
 export default {
   name: 'file-list',
@@ -36,98 +39,62 @@ export default {
     'cd-list-base': CDListBase,
     'notice-file': NoticeFile
   },
-  data (view) {
+  data () {
     return {
       url: '/mil/structure/forces/hydrographic/esim.htm',
-      links: [],
-      pages: [],
+      hrefs: [],
       error: AxiosError,
-      loaded: [{ innerText: '1', innerHTML: '1', href: '/mil/structure/forces/hydrographic/esim.htm', links: [] }]
+      pagesLoaded: [{ innerText: '1', innerHTML: '1', href: '/mil/structure/forces/hydrographic/esim.htm', links: [] }]
     }
   },
   computed: {
-    rowHref () {
-      return (row) => {
-        return `#${row.innerText}`
-      }
-    },
-    isLoaded ({ loaded }) {
-      return ({ innerHTML }) => loaded.findIndex(l => l.innerHTML === innerHTML) >= 0
-    },
-    currentPage ({ loaded }) {
-      return loaded.length - 1
-    },
-    resolveLinks ({ currentPage, loaded }) {
+    rowHref: () => ((row) => `#${row.innerText}`),
+    isLoaded: ({ pagesLoaded }) => (({ innerHTML }) => (pagesLoaded.findIndex(l => l.innerHTML === innerHTML) >= 0)),
+    currentPage: ({ pagesLoaded }) => pagesLoaded.length - 1,
+    current: ({ currentPage, pagesLoaded }) => pagesLoaded[currentPage],
+    reduced: ({ hrefs }) => ((hrefs
+        .filter(a => (a.classList.contains('pagenava') || 
+                            a.classList.contains('pagenav') || 
+                            a.classList.contains('dotted')) && 
+                            a.href.indexOf('esim') >= 0))
+        .reduce((r, i) => {
+          r[i.href] = r[i.href] || [];
+          r[i.href].push(i)
+          return r
+        }, Object.create(null))),
+    pagesResolved: ({ reduced, pagesLoaded }) => (Array.from(Object.keys(reduced)
+        .map(key => reduced[key].pop())
+        .map(a => ({
+          innerHTML: a.innerHTML.trim(),
+          innerText: a.innerText.trim(),
+          href: replaceMil(a.href),
+          params: Object.fromEntries((new URL(a.href)).searchParams.entries()),
+          links: []
+        }))
+        .filter(link => pagesLoaded.findIndex(l => l.innerText === link.innerText) < 0))
+        .filter(p => p.params.f !== undefined)),
+    pages: ({ pagesResolved, pagesLoaded }) => (pagesLoaded.concat(pagesResolved)),
+    resolveLinks ({ current }) {
       const list = this
       return (response) => {
-        const doc = domParser.parseFromString(response.data, 'text/html')
-        const hrefs = Array.from(doc.documentElement.querySelectorAll('a').values())
-        loaded[currentPage].links = hrefs
+        list.hrefs = Array.from(domParser.parseFromString(response.data, 'text/html').documentElement.querySelectorAll('a').values())
+        current.links = this.hrefs
           .filter(a => a.href.endsWith('pdf'))
-          .map(a => ({ href: a.href, innerHTML: a.innerHTML.replace('http://', 'https://').replace('https://structure.mil.ru', '/mil'), innerText: a.innerText }))
-        const _pages = hrefs.filter(a => (a.classList.contains('pagenava') || a.classList.contains('pagenav') || a.classList.contains('dotted')) && a.href.indexOf('esim') >= 0)
-          .reduce((r, i) => {
-            r[i.href] = r[i.href] || [];
-            r[i.href].push(i)
-            return r
-          } , Object.create(null))
-        list.pages = list.loaded.concat(Object.keys(_pages)
-          .map(p => _pages[p].pop())
-          .map(p => ({
-            innerHTML: p.innerHTML.trim(),
-            innerText: p.innerText.trim(),
-            href: p.href.replace('http://', 'https://').replace('https://structure.mil.ru', '/mil'),
-            params: Object.fromEntries((new URL(p.href)).searchParams.entries()),
-            links: []
-          })).filter(p => p.params.f !== undefined))
-        // this.pages = (((Object.keys(_pages)
-        //   .map(p => _pages[p].pop()))
-        //   .map(p => ({
-        //     innerHTML: p.innerHTML.trim(),
-        //     href: p.href.replace('http://', 'https://').replace('https://structure.mil.ru', '/mil'),
-        //     params: Object.fromEntries((new URL(p.href)).searchParams.entries()),
-        //     links: []
-        //   })))
-        //   .filter(p => p.params.f !== undefined))
+          .map(a => ({ 
+            href: a.href, 
+            innerHTML: replaceMil(a.innerHTML), 
+            innerText: a.innerText 
+          }))
       }
     }
   },
-  watch: {
-  },
   methods: {
-    // resolveLinks (response) {
-    //   const list = this
-    //     const doc = domParser.parseFromString(response.data, "text/html");
-    //     const nodes = doc.documentElement.querySelectorAll('a')
-    //     const hrefs = Array.from(nodes.values())
-    //     this.links.push(...((hrefs.filter(a => a.href.endsWith('pdf'))
-    //       .map(a => ({ href: a.href, innerHTML: a.innerHTML, innerText: a.innerText })))))
-    //     const _pages = hrefs.filter(a => (a.classList.contains('pagenava') || a.classList.contains('pagenav') || a.classList.contains('dotted')) && a.href.indexOf('esim') >= 0)
-    //       .reduce((r, i) => {
-    //         r[i.href] = r[i.href] || [];
-    //         r[i.href].push(i)
-    //         return r
-    //       } , Object.create(null))
-    //     this.pages = (((Object.keys(_pages)
-    //       .map(p => _pages[p].pop()))
-    //       .map(p => ({
-    //         innerHTML: p.innerHTML.trim(),
-    //         href: p.href,
-    //         params: Object.fromEntries((new URL(p.href)).searchParams.entries())
-    //       })))
-    //       .filter(p => p.params.f !== undefined))
-    // },
     onError (error, config) {
         this.error = error
     },
     getPage (event, { row }) {
-      this.loaded.push(row)
-      // this.url = row.href.replace('http://', 'https://').replace('https://structure.mil.ru', '/mil')
+      this.pagesLoaded.push(row)
     }
   }
 }
 </script>
-
-<style>
-
-</style>
